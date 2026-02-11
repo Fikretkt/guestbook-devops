ab55fb6f74089a17cd1ed4b781c285e8f4242d97
FikretKocatürk <fikretkctrkberlin@gmail.com>
2026-02-11 11:02:06 +0100

    Letzte Änderungen

--- init.sql
+++ init.sql
@@ -1,9 +1,10 @@
+-- Tabloyu oluşturuyoruz
 CREATE TABLE IF NOT EXISTS entries (
     id INT AUTO_INCREMENT PRIMARY KEY,
     guest_name VARCHAR(255) NOT NULL,
-    massage TEXT NOT NULL,
+    message TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO entries (guest_name, massage) VALUES ('Sistem', 'Ziyaretci defteri hazir!');
-)
+-- İlk veriyi ekliyoruz
+INSERT INTO entries (guest_name, message) VALUES ('Sistem', 'Ziyaretçi defteri hazir!');
